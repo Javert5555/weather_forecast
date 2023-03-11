8ee1b6864c6f4470e3696f6aee0ff7c61a950f39
Maxwell Rutherford <javert.hugo@yandex.ru>
2023-03-11 23:10:23 +0300

    make TodayWeatherData component

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,9 +1,14 @@
 import '../../styles/card/card.scss'
+import TodayWeatherCity from './TodayWeatherCity'
+import TodayWeatherData from './TodayWeatherData'
 
 const Card = () => {
     return (
         <div className='card'>
-            <div className='card__upper-part'></div>
+            <div className='card__upper-part'>
+                <TodayWeatherData />
+                <TodayWeatherCity />
+            </div>
             <div className='card__bottom-part'></div>
             <div></div>
         </div>
