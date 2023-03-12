b30a37822b0da7355b2b1836fb55d6f35d7462a7
Maxwell Rutherford <javert.hugo@yandex.ru>
2023-03-12 16:02:54 +0300

    make upper part card

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -4,14 +4,16 @@ import TodayWeatherData from './TodayWeatherData'
 
 const Card = () => {
     return (
-        <div className='card'>
-            <div className='card__upper-part'>
-                <TodayWeatherData />
-                <TodayWeatherCity />
+        <section className='container'>
+            <div className='card'>
+                <div className='card__upper-part'>
+                    <TodayWeatherData />
+                    <TodayWeatherCity />
+                </div>
+                <div className='card__bottom-part'></div>
+                <div></div>
             </div>
-            <div className='card__bottom-part'></div>
-            <div></div>
-        </div>
+        </section>
     )
 }
 
